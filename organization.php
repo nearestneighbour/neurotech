dd271ab974b5515143e81ef5267a285bf2fa2bd9
Tido Bergmans <tido_berg@hotmail.com>
2020-02-18 21:00:45 +0100

    first 'draft' for new pages (organization & NT promise) and updated menu

---
+++ organization.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML>
+<!--
+	Urban by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+<head>
+	<title>Our Mission - NeurotechEU</title>
+	<link rel="icon" href="images/icon.png" />
+
+	<meta name="description" content="The goal of NeurotechEU is to build a trans-European network of excellence in brain research and technologies to increase the competitiveness of European education, research, economy and society." />
+	<meta name="robots" content="index,follow" />
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="css/main.css" />
+</head>
+<body class="subpage">
+
+<!-- Header -->
+<?php
+include "html/header.html";
+?>
+
+<!-- Main -->
+<div id="main">
+
+<!-- Section -->
+<section class="wrapper pagetop">
+	<div class="inner">
+		<header class="align-center">
+			<h1>Our Mission</h1>
+			<b><p>Build a trans-European network of excellence in brain research and technologies<br>to increase the competitiveness of European education, research, economy and society</p></b>
+		</header>
+	</div>
+</section>
+
+
+<section class="wrapper style2">
+	<div class="inner">
+		<div class="flex flex-2">
+			<div class="col col1">
+				<div class="image round fit">
+					<img src="images/workspace.jpg" alt="Students working together in a digital campus"/>
+				</div>
+			</div>
+			<div class="col col2 padtop">
+				<h2>Neurotech<sup>EU</sup> Campus+</h2>
+				Neurotech<sup>EU</sup> CAMPUS+ is a digital campus where students can
+				<ul><li>Learn about courses on offer</li>
+				<li>Follow online courses</li>
+				<li>Meet with their peers</li>
+				<li>Organize and participate in study groups</li>
+				<li>Perform administrative tasks</li>
+				<li>Manage their blended learning experience</li></ul>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="wrapper style1">
+	<div class="inner">
+		<div class="flex flex-2">
+			<div class="col col2 padtop">
+				<h2>Neurotech<sup>EU</sup> Graduate School</h2>
+				Neurotech<sup>EU</sup> Graduate School will train students in an international and intersectoral setting.
+				<ul><li>Each student will be mentored by 3 mentors; two from partnering universities, the third from the industrial or societal partners</li>
+				<li>Each student will receive full fellowship</li>
+				<li>Admissions to this top-tier graduate school will be highly competitive and centrally organized</li>
+				<li>By graduation each student will have an intersectoral experience with a strong multidisciplinary research background</li></ul>
+			</div>
+			<div class="col col1">
+				<div class="image round fit">
+					<img class="link" src="images/grad.jpg" alt="A graduate student from the Neurotech graduate school"/>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="wrapper style2">
+	<div class="inner">
+		<div class="flex flex-2">
+			<div class="col col1 first">
+				<div class="image round fit">
+					<img src="images/reschooling.jpg" alt="People studying at the Neurotech Lifelong Learning Center"/>
+				</div>
+			</div>
+			<div class="col col2 padtop">
+				<h2>Neurotech<sup>EU</sup> Lifelong Learning Center</h2>
+				Neurotech<sup>EU</sup> Lifelong Learning Center will provide training for personal development after formal education. It will:
+				<ul><li>focus on the necessary knowledge, skill sets, and competencies for continued learning throughout life</li>
+				<li>Help individuals to adapt to the changing personal, civic, societal and employment related needs</li>
+				<li>Deliver the necessary education and experience for individuals to pursue opportunities in brain research and technologies</li>
+				<li>Organize certificate programs, micro-degrees, boot-camps, provide internship, research opportunities and transferable skills</li>
+			</div>
+		</div>
+	</div>
+</section>
+
+</div>
+
+<!-- Footer -->
+<?php
+include 'html/footer.html';
+?>
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.scrolly.min.js"></script>
+<script src="js/jquery.scrollex.min.js"></script>
+<script src="js/skel.min.js"></script>
+<script src="js/util.js"></script>
+<script src="js/main.js"></script>
+
+<script src="https://kit.fontawesome.com/12081b1371.js"></script>
+
+</body>
+</html>
