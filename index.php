6525cbc40eead435a703781be830ce0c5677e3c3
Tido Bergmans <tido_berg@hotmail.com>
2020-02-27 09:12:01 +0100

    changed no. of associates stat

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ echo $doc->saveHTML();
 			<li><a href="synergy#funding"><i class="fa fa-3x fa-microscope"></i><p>171</p><p>Joint grants</p></a></li>
 			<li><a href="synergy#funding"><i class="fa fa-3x fa-euro"></i><p>€231M</p><p>Joint funding</p></a></li>
 			<li><a href="synergy#publications"><i class="fa fa-3x fa-newspaper"></i><p>5277</p><p>Joint publications</p></a></li>
-			<li><i class="fa fa-3x fa-globe"></i><p>100+</p><p>Affiliates</p></li>
+			<li><i class="fa fa-3x fa-globe"></i><p>200+</p><p>Affiliates</p></li>
 		</ul>
 		<a href="#main" class="button big scrolly">Learn More</a>
 	</div>
