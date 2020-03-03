5f9fcd6892e1382cdfaa03b08272c4a0ccf94faa
Tido Bergmans <tido_berg@hotmail.com>
2020-03-03 11:35:50 +0100

    organization page; fixed organigram img, vertically centered content

--- organization/index.php
+++ organization/index.php
@@ -58,7 +58,7 @@
 		</div>
 	</section>
 
-<section class="wrapper style2">
+<section class="wrapper style2 organization">
 	<div class="inner">
 		<div class="flex flex-2">
 			<div class="col first">
@@ -66,21 +66,21 @@
 					<img src="/assets/images/workpackages.jpg" alt="Work packages of the Neurotech project"/>
 				</div>
 			</div>
-			<div class="col">
+			<div class="col"><div>
 				<b class="redbold">A central goal of Neurotech<sup>EU</sup> activities is the continuous integration of education, research, and innovation.</b> Therefore, the four content delivery work packages, i.e. Neurotech<i>2040</i> (WP3), Neurochallenges in Education & Research (WP4), Technological Innovation (WP5) and Societal Innovation (WP6) are organizationally interlinked while they all work together to facilitate education, training, and life-long learning. The remaining four WPs WP1-2 will plan, organize, command, coordinate and control the Alliance actions, maximizing the utilization of resources and human capital to ensure organizational success. WP7 and WP8 will widen the participation, spread excellence in education and research in a sustainable fashion; they will ensure that Neurotech<sup>EU</sup> actions, outcomes, competencies, and know-how will be disseminated across sectors throughout Europe.
-			</div>
+			</div></div>
 		</div>
 	</div>
 </section>
-<section class="wrapper style1">
+<section class="wrapper style1 organization">
 	<div class="inner">
 		<div class="flex flex-2">
-			<div class="col">
+			<div class="col"><div>
 				The Alliance provides a structural and sustainable approach to the inclusion of all voices on campus  (bachelor’s, master’s and doctoral students, life-long learners, teaching and research staff, support and administrative staff, management positions) and off campus (public policy-makers, associate partners, associated (European) universities, industrial and innovation partners, regional stakeholders, governmental and non-governmental organizations, and society at large). The cooperation across geographically fragmented teams will be upscaled by creating a shared digital tool for virtual collaboration and organizing meetings for direct interaction with all stakeholders, creating a melting-pot of cultures, ideas and talent.
-			</div>
+			</div></div>
 			<div class="col first">
 				<div class="image fit">
-					<img src="/assets/images/organigram.jpg" alt="Organizational structure diagram of NeurotechEU university"/>
+					<img src="/assets/images/organigram.png" alt="Organizational structure diagram of NeurotechEU university"/>
 				</div>
 			</div>
 		</div>
